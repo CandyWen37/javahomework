c709e4cb4d283020f27528e6bc4b3b7c53d5a99a
CandyWen37 <70477928+CandyWen37@users.noreply.github.com>
2020-10-22 00:03:01 +0800

    Add files via upload

---
+++ homework8_2.java
@@ -0,0 +1,24 @@
+package homework;
+import java.util.*;
+import java.io.*;
+
+public class homework8_2 {
+    public static void main(String[] args)
+            throws Exception
+    {
+        Scanner input = new Scanner(System.in);
+        double[][] test = new double[4][4];
+        System.out.println("Enter a 4-by-4 matrix row by row:");
+        for(int i=0;i<4;i++)
+            for(int j=0;j<4;j++)
+                test[i][j]=input.nextDouble();
+        System.out.println("Sum of the elements in the major diagonal is "+sumMajorDiagonal(test));
+    }
+    public static double sumMajorDiagonal(double[][] m){
+        int len = m.length;
+        double sum = 0.0;
+        for(int i=0;i<len;i++)
+            sum+=m[i][i];
+        return sum;
+    }
+}
